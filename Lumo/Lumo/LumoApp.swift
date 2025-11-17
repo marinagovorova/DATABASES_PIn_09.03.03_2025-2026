19f8c10727ddca40d29bac8353569a27baf58db7
Elizaveta <liza.plisiuk@gmail.com>
2025-11-17 13:57:56 +0300

    Xcode project added

---
+++ Lumo/Lumo/LumoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumoApp.swift
+//  Lumo
+//
+//  Created by Лиза Плисюк on 17.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct LumoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
